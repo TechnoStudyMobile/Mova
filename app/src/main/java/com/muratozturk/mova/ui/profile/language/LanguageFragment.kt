39deca005c125cf7fd441c85231b66040b9574de
Mehmet Ali Kılıç <115002434+kilicma@users.noreply.github.com>
2023-06-15 19:50:01 +0300

    Unused methods have been removed. Codes updated.

--- app/src/main/java/com/muratozturk/mova/ui/profile/language/LanguageFragment.kt
+++ app/src/main/java/com/muratozturk/mova/ui/profile/language/LanguageFragment.kt
@@ -22,21 +22,11 @@ class LanguageFragment : Fragment(R.layout.fragment_language) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initUI()
         collectData()
         binding.backButton.setOnClickListener {
             findNavController().popBackStack()
         }
     }
-
-    fun initUI() {
-        with(binding) {
-            with(viewModel) {
-
-            }
-        }
-    }
-
     fun onClick(language: String, code: String) {
         viewModel.setLanguage(language)
         viewModel.setLanguageCode(code)
